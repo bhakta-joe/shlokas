edfa1d4e9b348f00e5e09e4eaaa78cd618aeb152
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-08-08 09:25:28 +0000

    forceOwnership = true

--- src/services/data.ts
+++ src/services/data.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { enableIndexedDbPersistence } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +22,9 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export let dbError = ""
 
-enableIndexedDbPersistence(db)
+enableIndexedDbPersistence(db,
+  { forceOwnership: true }
+)
   .catch((err) => {
     dbError = err
     console.log(err)
